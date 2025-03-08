32b4790a5ea7fdcaaa718c124d9a6d9c9e7a2d4f
ulaiyya <ulaiyyamehdi@gmail.com>
2025-03-08 16:09:20 +0500

    basic-setup

---
+++ src/app/show-student/show-student.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-student',
+  templateUrl: './show-student.component.html',
+  styleUrl: './show-student.component.css'
+})
+export class ShowStudentComponent {
+
+}
