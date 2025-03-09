7122464b0ce67fbd1fd5456918f56c0f021d05d2
ulaiyya <ulaiyyamehdi@gmail.com>
2025-03-09 12:07:08 +0500

    basic-setup

--- src/app/add-student/add-student.component.ts
+++ src/app/add-student/add-student.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-student',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './add-student.component.css'
 })
 export class AddStudentComponent {
+
+  constructor ( private httpclient:HttpClient ) {  }
+
+  student = new FormGroup({
+    rollNo: new FormControl(),
+    name: new FormControl(),
+    percentage: new FormControl(),
+    branch: new FormControl()
+  });
+
+  handleSubmit(){
+    const url = "http://localhost:8080/student/add"
+
+    console.log(this.student.value);
+    
+    this.httpclient.post(url, this.student.value).subscribe((response) => {
+      console.log(response);
+    }, (error) => {
+      console.error("Error in Adding Record", error);
+    }
+  )
+  }
 
 }
--- src/app/show-student/show-student.component.ts
+++ src/app/show-student/show-student.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-show-student',
   templateUrl: './show-student.component.html',
   styleUrl: './show-student.component.css'
 })
-export class ShowStudentComponent {
+export class ShowStudentComponent implements OnInit{
+
+  constructor ( private httpclient : HttpClient ) {  }
+
+  ngOnInit(): void {
+      this.getAllStudentData();
+  }
+
+  students: any[] = [];
+
+  getAllStudentData(){
+    const url = "http://localhost:8080/students"
+
+    this.httpclient.get(url).subscribe((response:any) => {
+      console.log(response);
+      this.students = response;
+    }, (error) => {
+      console.error("Error in Fetching Record", error);
+    })};
+
+    deleteStudent(studentId:any) {
+      const url = `http://localhost:8080/student/delete/${studentId}`
+
+      this.httpclient.delete(url).subscribe((response: any) => {
+        console.log(response);
+        this.getAllStudentData();
+      }, (error) => {
+        console.error("Error in Deleting Record", error);
+      })
+
+    }
 
 }
